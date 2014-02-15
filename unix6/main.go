56e462afec6a280272b52f5ab04b3df35aac6de3
Dave Cheney <dave@cheney.net>
2014-02-15 14:06:52 +1100

    Make BOOTRK05 a map

--- unix6/main.go
+++ unix6/main.go
@@ -10,11 +10,9 @@ import (
 )
 
 func stdin(c chan uint8) {
-	c <- 'u'
-	c <- 'n'
-	c <- 'i'
-	c <- 'x'
-	c <- '\n'
+	for _, v := range []byte("unix\n") {
+		c <- v
+	}
 	var b [1]byte
 	for {
 		n, err := os.Stdin.Read(b[:])
@@ -29,9 +27,8 @@ func stdin(c chan uint8) {
 
 func main() {
 	pdp := pdp11.New()
-	const LOADADDR = 01000
-	pdp.LoadBootrom(LOADADDR, pdp11.BOOTRK05)
-	pdp.SetPC(LOADADDR*2 + 2)
+	pdp.LoadMemory(pdp11.BOOTRK05)
+	pdp.SetPC(002000)
 	pdp.Attach(0, filepath.Join(build.Default.GOPATH, "src/github.com/davecheney/pdp11/rk0"))
 	go stdin(pdp.Input)
 	pdp.Run()
